8ffba0b7a1e42bdc5e169c32ada47b803bd03291
Torsten Werner <torsten.werner@westernacher.com>
2016-12-15 22:25:42 +0100

    initial import

---
+++ src/main/java/com/westernacher/SampleCiCdApplication.java
@@ -0,0 +1,12 @@
+package com.westernacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCiCdApplication.class, args);
+	}
+}
---
+++ src/main/java/com/westernacher/SampleController.java
@@ -0,0 +1,12 @@
+package com.westernacher;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
